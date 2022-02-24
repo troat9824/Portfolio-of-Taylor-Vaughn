7c7d5dd475d96735d1f3df9f9982cb9e3aabe3e0
troat9824 <troat9824@gmail.com>
2022-02-24 06:41:17 -0600

    adding javascript

---
+++ assets/app.js
@@ -0,0 +1,45 @@
+// select the pages
+var headerPage = document.querySelector('.header');
+var aboutMePage = document.querySelector('.about-me-page');
+var workIveDonePage = document.querySelector('.Work-ive-done');
+var contactMePage = document.querySelector('.Contact-info-page');
+
+// get the buttons
+var aboutMeLink = document.getElementById('about-me-link');
+var workIveDoneLink = document.getElementById('work-ive-done-link');
+var contactMeLink = document.getElementById('contact-info-link')
+
+// start with just header page
+function enterPage() {
+    headerPage.style.display = "block";
+    aboutMePage.style.display = "none";
+    workIveDonePage.style.display = "none";
+    contactMePage.style.display = "none";
+
+    
+};
+function aboutMe() {
+    headerPage.style.display = "none";
+    aboutMePage.style.display = "block";
+    workIveDonePage.style.display = "none";
+    contactMePage.style.display = "none";
+};
+
+function workIveDone() {
+    headerPage.style.display = "none";
+    aboutMePage.style.display = "none";
+    workIveDonePage.style.display = "block";
+    contactMePage.style.display = "none";
+};
+
+function contactMe() {
+    headerPage.style.display = "none";
+    aboutMePage.style.display = "none";
+    workIveDonePage.style.display = "none";
+    contactMePage.style.display = "block";
+}
+
+enterPage();
+aboutMeLink.addEventListener('click', aboutMe);
+workIveDoneLink.addEventListener('click', workIveDone);
+contactMeLink.addEventListener('click', contactMe);
